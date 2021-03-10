fd34ec9e406a6986295be91c40539375f91f5912
JihaoWu <871070853@qq.com>
2021-03-10 16:56:14 +0800

    9th

--- Test1/test1/test1/test2.c
+++ Test1/test1/test1/test2.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
+#include<windows.h>
+#include<stdlib.h>
 
 //if语句
 //int main() {
@@ -298,32 +300,68 @@
 //	return 0;
 //}
 
-//折半查找
+// 折半查找/二分查找
+//int main() {
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int left = 0;
+//	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+//	int key = 7;
+//	int mid = 0;
+//	while (left <= right) {
+//		
+//		mid = (left + right) / 2;
+//
+//		if (arr[mid] > key) {
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < key) {
+//			left = mid + 1;
+//		}
+//		else
+//			break;
+//	}
+//	if (left<=right) {
+//		printf("找到了，下标是%d\n", mid);
+//	}
+//	else {
+//		printf("没找到！\n");
+//	}
+//	return 0;
+//}
+
+//实现一个二分查找函数
+//int bin_search(int ) {
+//
+//}
 
 
-//代码1 //编写代码，演示多个字符从两端移动，向中间汇聚 #include <stdio.h>
+
+//编写代码，演示多个字符从两端移动，向中间汇聚 #include <stdio.h>
 //int main()
 //{
-//    char arr1[] = "welcome to bit..."; char arr2[] = "#################"; int left = 0;
-//    int right = strlen(arr1) - 1; printf("%s\n", arr2); //while循环实现
-//    while (left <= right)
-//    {
-//        Sleep(1000);
-//        arr2[left] = arr1[left];
-//        arr2[right] = arr1[right];
-//        left++;
-//        right--;
-//        printf("%s\n", arr2);
-//    }
-//    //for循环实现
-//    for (left = 0, right = strlen(src) - 1;
-//        left <= right;
-//        left++, right--)
-//    {
-//        Sleep(1000);
-//        arr2[left] = arr1[left];
-//        arr2[right] = arr1[right];
-//        printf("%s\n", target);
-//    }
-//    retutn 0;
+//    char arr1[] = "Hello Mr.Wu , Welcome to the code world...";
+//    char arr2[] = "##########################################";
+//    int left = 0;
+//    int right = strlen(arr1) - 1;//int right = sizeof(arr1) / sizeof(arr1[0]) - 2;
+//    printf("%s\n", arr2);
+    //while (left <= right)//while循环实现
+    //{
+    //    Sleep(200);//休息0.2s,sleep单位是ms;
+    //    system("cls");//执行系统命令-cls-清空屏幕
+    //    arr2[left] = arr1[left];
+    //    arr2[right] = arr1[right];
+    //    left++;
+    //    right--;
+    //    printf("%s\n", arr2);
+    //}
+    ////for循环实现
+    /*for (left = 0, right = strlen(arr1) - 1;left <= right;left++, right--)
+    {
+        Sleep(200);
+        system("cls");
+        arr2[left] = arr1[left];
+        arr2[right] = arr1[right];
+        printf("%s\n", arr2);
+    }
+    return 0;*/
 //}
